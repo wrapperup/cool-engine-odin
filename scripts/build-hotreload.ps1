e683b612263c0db4bc1e7505d4ff7b146b4c6a77
wrapperup <wrapperup4@gmail.com>
2024-12-20 03:09:27 -0500

    fix linker flag regression

--- scripts/build-hotreload.ps1
+++ scripts/build-hotreload.ps1
@@ -1,2 +1,2 @@
-odin build src -build-mode:dll -out:build/debug/game.dll -collection:deps=deps -ignore-unknown-attributes -debug -o:none -use-separate-modules -show-timings
+odin build src -build-mode:dll -out:build/debug/game.dll -collection:deps=deps -ignore-unknown-attributes -debug -o:none -use-separate-modules -show-timings -linker:lld
 exit $LastExitCode
